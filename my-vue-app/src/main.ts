afa046070689f6e550c8e356e4451804dab1d218
aliyu <1324149139@qq.com>
2023-01-30 16:53:15 +0800

    学习 泛型 | extends

--- my-vue-app/src/main.ts
+++ my-vue-app/src/main.ts
@@ -6,5 +6,28 @@
 // const currentWith = {a: 1} satisfies IConfig
 // console.log(currentWith.a)
 // console.log(currentWith.a.toFixed())
+interface User{
+    readonly id: string | number
+    name: string
+    age: number
+}
 
+type Dog = {
+    name: string
+}
+// interface UserWithDog extends User {
+//     pet: Dog
+// }
+
+interface UserWithPet<T> extends  User {
+    pet: T
+}
+
+const u1: UserWithPet<Dog> = {
+    id: 1,
+    name: 'John',
+    age: 20,
+    pet: {name: 'Rex'}
+}
+console.log(u1.pet?.name)
 export {}
